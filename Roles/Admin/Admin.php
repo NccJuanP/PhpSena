8fa8966060927669e6573a4156bdaa50a76b3f1a
Juan Pablo <juanpablolc414@gmail.com>
2024-05-17 07:40:18 -0500

    clase 8, distinguir roles

---
+++ Roles/Admin/Admin.php
@@ -0,0 +1,25 @@
+<?php
+//seguridad de sessiones paginacion 
+session_start();
+error_reporting(0);
+$varsesion = $_SESSION['usuario'];
+if (!$varsesion) {
+    header("location: ../index.html");
+    die();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1> hola admin</h1>
+    <a href="../CerrarSesion.php">Cerrar sesión</a>
+</body>
+
+</html>
---
+++ Roles/src/Data/DB.php
@@ -0,0 +1,8 @@
+<?php 
+    session_start();
+
+    $conn = mysqli_connect("bfv2kdcjllxqqdgnyepd-mysql.services.clever-cloud.com","us2q7mwcrptzmv3f","t0Qk8KhPjNm56ZgRKTw8","bfv2kdcjllxqqdgnyepd");
+    /* if (isset($conn)) {
+        echo "DB connect";
+    } */
+?>
